b6c77a198b25454a2c71819af3e02923d328ac99
Temitope Abdulyekeen Oyeyemi <temitope.oyeyemi101@gmail.com>
2025-05-22 15:56:24 +0100

    Content Security Policy

--- myApp.js
+++ myApp.js
@@ -12,7 +12,9 @@ app.use(helmet.hsts({maxAge: timeInSeconds , force: true} ));
 //helmet.dnsPrefetchControl()
 app.use(helmet.dnsPrefetchControl());
 app.use(helmet.noCache());
-
+app.use(helmet.contentSecurityPolicy({directives: {
+  defaultSrc: ["'self'"],
+  scriptSrc: ["'self'", 'trusted-cdn.com'],}}));
 
 
 
