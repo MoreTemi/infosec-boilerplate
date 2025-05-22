56c7aebbe4f8ae7616076fef1751606e64eacad8
Temitope Abdulyekeen Oyeyemi <temitope.oyeyemi101@gmail.com>
2025-05-22 16:33:22 +0100

    Using parent helmet

--- myApp.js
+++ myApp.js
@@ -2,22 +2,33 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 const PORT = process.env.PORT || 3000; 
-app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({ action: 'deny' }));
-app.use(helmet.xssFilter());
-app.use(helmet.noSniff());
-app.use(helmet.ieNoOpen());
-const timeInSeconds = 90*24*60*60;
-app.use(helmet.hsts({maxAge: timeInSeconds , force: true} ));
-//helmet.dnsPrefetchControl()
-app.use(helmet.dnsPrefetchControl());
-app.use(helmet.noCache());
-app.use(helmet.contentSecurityPolicy({directives: {
-  defaultSrc: ["'self'"],
-  scriptSrc: ["'self'", 'trusted-cdn.com'],}}));
+// app.use(helmet.hidePoweredBy());
+// app.use(helmet.frameguard({ action: 'deny' }));
+// app.use(helmet.xssFilter());
+// app.use(helmet.noSniff());
+// app.use(helmet.ieNoOpen());
+// const timeInSeconds = 90*24*60*60;
+// app.use(helmet.hsts({maxAge: timeInSeconds , force: true} ));
+// //helmet.dnsPrefetchControl()
+// app.use(helmet.dnsPrefetchControl());
+// app.use(helmet.noCache());
+// app.use(helmet.contentSecurityPolicy({directives: {
+//   defaultSrc: ["'self'"],
+//   scriptSrc: ["'self'", 'trusted-cdn.com'],}}));
 
 
+app.use(helmet({
 
+  //frameguard: { action: 'deny' },
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ["'self'",'trusted-cdn.com'],
+    },
+  },
+  //dnsPrefetchControl: false,
+  noCache: true
+}));
 
 
 
